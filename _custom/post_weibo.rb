ff081e7e8f769660cf496e7a29eadac8fbcbf42a
agiledon <bruce.e.zhang@gmail.com>
2012-12-25 15:05:11 +0800

    add post_weibo.rb and Rakefile to support post sina weibo

---
+++ _custom/post_weibo.rb
@@ -0,0 +1,48 @@
+require 'faraday'
+require 'yaml'
+
+class WeiboPoster
+  def initialize
+    @config = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../_config.yml'))    
+  end
+
+  def post_weibo
+    conn = Faraday.new(:url => "https://api.weibo.com")
+
+    result = conn.post '/2/statuses/update.json',
+                       :access_token => @config['access_token'],
+                       :status => generate_post
+    puts "post successful"
+  end
+
+  private
+
+  def generate_post    
+    post_template = @config['post_template'].force_encoding("utf-8")
+    post_template % {:blog_title => latest_blog_title, :blog_url => generate_blog_url }   
+  end
+
+  def latest_blog_title
+    title_line = IO.readlines(latest_blog_file_name)[2]
+    title_line["title: ".length + 1..title_line.length - 3].force_encoding("utf-8")
+  end
+
+  def latest_blog_file_name
+    blogs_path = File.expand_path(File.dirname(__FILE__) + '/../source/_posts')
+    filtered_right_blog = Dir.glob(blogs_path + "/*").select{|f| f.match(/\.markdown/)}
+    filtered_right_blog.max_by {|f| File.mtime(f)}
+  end
+
+  def generate_blog_url    
+    full_url = @config['url'] + "/blog/" + convert_to_blog_url(latest_blog_file_name)
+    full_url.force_encoding("utf-8")
+  end
+
+  def convert_to_blog_url(post_file_name)
+    #convert 2012-12-21-demo-blog.markdown file name to be normal blog url: 2012/12/21/demo-blog
+    File.basename(post_file_name, ".markdown").gsub(/\d-/) { |s| s[0] + "/"}
+  end
+end
+
+poster = WeiboPoster.new
+poster.post_weibo
